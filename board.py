903ce5cf74c2697570c118246b1cddd4cfdff61a
wzh <414266283@qq.com>
2023-03-26 21:55:12 +0800

    update list container

--- board.py
+++ board.py
@@ -5,14 +5,16 @@ from soduku import *
 class board :
 
     def __init__(self, root,row,colum):
-        root.title("Feet to Meters")
+        root.title("Soduku")
 
         self.row = row
         self.collum = colum
+
+        self.sodukus = []
 
         ## create the soduku
         for i in range(0,row) :
             for j in range(0,colum) :
                 iter = soduku(root,row,colum)
                 iter.grid(colum = j*colum,row = i*row,columspan = colum,rowspan = row)
-                self.sodukus[i][j] = iter
+                self.sodukus.append(iter)
